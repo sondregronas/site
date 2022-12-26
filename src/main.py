b2dd9a6fbbe505ffa369f62d753b16eea1a60c66
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-12-26 13:57:53 +0100

    :fire: Remove javascript and @import statement

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 from __init__ import write_html, public_to_dist
+from datetime import datetime
 
 pages = {
     'views/index.pug': {},
@@ -9,6 +10,7 @@ metadata = {
     'title': 'Sondre Grønås',
     'favicon': 'favicon.ico',
     'description': 'Sondre Grønås is a passionate ICT-educator and media engineer from Norway.',
+    'year': datetime.now().year,
 }
 
 public_to_dist()
