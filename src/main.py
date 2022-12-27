bc8882b42e05a2d586e6f58017167bc628a7f9d1
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-12-27 22:42:45 +0100

    :question: Add 404 page

--- src/main.py
+++ src/main.py
@@ -4,16 +4,20 @@ from datetime import datetime
 from __init__ import write_html, public_to_dist, get_clients_data
 
 BASE_URL = 'https://sondregronas.github.io/sondregronas.com'
+SITE_NAME = 'Sondre Grønas'
 
 PAGES = {
     'views/index.pug': {},
-    'views/clients.pug': {'clients': get_clients_data(),
+    'views/404.pug': {'title': '404 - Page not found',
+                      'og_description': 'Page not found'},
+    'views/clients.pug': {'title': f'Clients - {SITE_NAME}',
+                          'clients': get_clients_data(),
                           'og_description': 'Meet our clients, are you next?'},
 }
 
 metadata = {
-    'title': 'Sondre Grønås',
     'base_url': BASE_URL,
+    'title_fallback': SITE_NAME,
     'og_image_fallback': f'{BASE_URL}/logo.png',
     'og_description_fallback': 'Sondre Grønås is a passionate ICT-educator and media engineer from Norway.',
     'year': datetime.now().year,
