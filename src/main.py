bcb9ffa54803ce2b8d91117e28c2d465024d23ea
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-12-29 12:53:29 +0100

    :pencil: Fix name in reference link, adjust responsive padding

--- src/main.py
+++ src/main.py
@@ -8,18 +8,25 @@ SITE_NAME = 'Sondre Grønas'
 
 PAGES = {
     'views/index.pug': {},
+    # 404 page
     'views/404.pug': {'title': '404 - Page not found',
                       'og_description': 'Page not found'},
+    # clients
     'views/clients.pug': {'title': f'Clients - {SITE_NAME}',
                           'clients': get_clients_data(),
                           'og_description': 'Meet our clients, are you next?'},
+    # podcasts
     'views/podcasts.pug': {'title': f'Podcasts - {SITE_NAME}',
                            'podcasts': get_podcasts_data(),
                            'og_description': 'Sondre Grønås has worked with a lot of podcasts, here are some of them.'},
-    'views/old-content/managing-postgresql-on-a-synology-server.pug': {
+    # Legacy articles - might be removed in the future
+    'views/legacy/managing-postgresql-on-a-synology-server.pug': {
         'title': 'Deprecated - Managing PostgreSQL on a Synology server',
         'url': 'managing-postgresql-on-a-synology-server',
-        'og_description': 'Deprecated article - this article has been removed. Sorry for the inconvenience.'},
+        'og_description': 'Deprecated article - the article contents have been removed. '
+                          'Sorry for the inconvenience. I recommend you check out this docker container by '
+                          'Elliot Mason instead, as it\'s a better solution than the one I wrote about here: '
+                          'https://github.com/elliotmatson/Docker-Davinci-Resolve-Project-Server'},
 }
 
 metadata = {
